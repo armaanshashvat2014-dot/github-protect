b05d8b5d063bdade7a1b5fbbbb584cbd78e187e0
Armaan <armaan.shashvat2014@gmail.com>
2026-09-20 20:33:04 +0530

    Refresh traffic hub assets after challenge fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='github-protect-v34';
+const CACHE='github-protect-v35';
 const CORE=['./','./index.html','./manifest.json','./icon.svg','./latest-version.json','./landing.css','./landing.js','./translator.js','./pro-upgrade.js','./traffic-engine.js','./pro/','./teams/'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
