10f33a510b6bb0d3b0abc20670c356cc320cab0f
Armaan <armaan.shashvat2014@gmail.com>
2026-09-20 13:12:53 +0530

    Launch GitHub Protect Pro workspace preview v5.2.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='github-protect-v31';
-const CORE=['./','./index.html','./manifest.json','./icon.svg','./latest-version.json','./landing.css','./landing.js','./translator.js'];
+const CACHE='github-protect-v32';
+const CORE=['./','./index.html','./manifest.json','./icon.svg','./latest-version.json','./landing.css','./landing.js','./translator.js','./pro-upgrade.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
