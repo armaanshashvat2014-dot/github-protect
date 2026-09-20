2c8a9dbb4eee790519b21479a53a9fe5095e10d8
Armaan <armaan.shashvat2014@gmail.com>
2026-09-20 20:28:35 +0530

    Add privacy-safe traffic growth hub v5.4.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='github-protect-v33';
-const CORE=['./','./index.html','./manifest.json','./icon.svg','./latest-version.json','./landing.css','./landing.js','./translator.js','./pro-upgrade.js','./pro/','./teams/'];
+const CACHE='github-protect-v34';
+const CORE=['./','./index.html','./manifest.json','./icon.svg','./latest-version.json','./landing.css','./landing.js','./translator.js','./pro-upgrade.js','./traffic-engine.js','./pro/','./teams/'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
